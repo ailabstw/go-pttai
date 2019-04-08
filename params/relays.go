b9872c51eb4b9e604ef1b4d6a3a51aabcadb421a
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2019-04-08 18:12:58 +0800

    public p2p-relays

--- params/relays.go
+++ params/relays.go
@@ -16,4 +16,8 @@
 
 package params
 
-var P2PRelays = []string{}
+var MainP2PRelays = []string{}
+
+var TestP2PRelays = []string{
+	"pnode://16Uiu2HAmBnH6A6fBUbKVWsAwKE3FwASmQyRu93K3ZugZL1yzVpZt@3.1.79.86:9487",
+}
